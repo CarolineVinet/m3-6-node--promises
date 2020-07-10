6c5d86eceb868f64c846a6e2fd617be289ec8bad
Caroline Vinet <carovinet@gmail.com>
2020-07-10 15:54:00 -0400

    exercise 2.2

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -2,18 +2,21 @@
 // ----------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
 const greeting = (langCode) => {
-  return request('<URL>') // 1
-    .then((response) => JSON.parse(response))
-    .then((parsedResponse) => {
-      return; // 2
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`)
+    .then((response) => {
+      const data = JSON.parse(response);
+      return data;
     })
-    .catch((err) => console.log('Error: ', err));
+    .then((parsedResponse) => {
+      return parsedResponse.data;
+    })
+    .catch((err) => console.log("Error: ", err));
 };
 
 // Testing
-greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+greeting("da").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
 
 // 3
